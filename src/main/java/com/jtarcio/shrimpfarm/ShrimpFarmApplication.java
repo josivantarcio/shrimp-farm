84b689938197d0b03d9be0a34a4e54455340918f
Josivan Tarcio <josivantarcio@gmail.com>
2025-11-28 13:22:02 -0300

    first commit

---
+++ src/main/java/com/jtarcio/shrimpfarm/ShrimpFarmApplication.java
@@ -0,0 +1,13 @@
+package com.jtarcio.shrimpfarm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShrimpFarmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShrimpFarmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jtarcio/shrimpfarm/ShrimpFarmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jtarcio.shrimpfarm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShrimpFarmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
